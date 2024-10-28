3a7c3366ec19258709fec756ceab3929de092246
Tanjilul Haq <thrafi16@gmail.com>
2024-10-28 22:15:04 +0600

    Added live life and score

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,47 @@
+function handleKeyboard(event){
+    const playerPressed = event.key;
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet)
+
+    if ( playerPressed === expectedAlphabet){
+        const currentScoreElem = document.getElementById('current-score');
+        const currentScoreText = currentScoreElem.innerText;
+        const currentScore = parseInt(currentScoreText);
+        const newScore = currentScore+1;
+        currentScoreElem.innerText = newScore;
+        removeBgColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        const currentLifeElem = document.getElementById('current-life');
+        const currentLifeText = currentLifeElem.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const newLife = currentLife-1;
+
+
+        currentLifeElem.innerText = newLife;
+    }
+}
+
+document.addEventListener('keyup', handleKeyboard);
+
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    console.log('your alphabet', alphabet);
+
+    const currentAlphabet = document.getElementById('current-alphabet');
+    currentAlphabet.innerHTML = alphabet;
+    addBgColorById(alphabet);
+}
+
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame();
 }
+
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,25 @@ function showElementById(elementid){
     const element = document.getElementById(elementid);
     element.classList.remove('hidden');
 }
+
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    const randomNum = Math.random()*25;
+    const index = Math.round(randomNum);
+
+    const alphabet = alphabets[index];
+    return alphabet;
+    
+}
+
+function addBgColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+function removeBgColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
